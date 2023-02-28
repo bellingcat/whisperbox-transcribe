4eed77bfa7d63f6230248eefcdf1d5412591a1c9
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-02-28 20:20:51 +0100

    fix: enable sqlite WAL

--- app/shared/db/base.py
+++ app/shared/db/base.py
@@ -1,11 +1,20 @@
-from typing import Generator
+from typing import Any, Generator
 
-from sqlalchemy import create_engine
+from sqlalchemy.engine import Connection
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import Session, sessionmaker
 
 from app.shared.settings import settings
 
 engine = create_engine(settings.DATABASE_URI, connect_args={"check_same_thread": False})
+
+
+@event.listens_for(engine, "connect")
+def set_sqlite_pragma(conn: Connection, _: Any) -> None:
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA journal_mode=WAL")
+    cursor.close()
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
