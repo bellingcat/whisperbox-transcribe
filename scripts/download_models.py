136852afda9b438deafe45f4e42fbae99b9ea98f
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-06-28 11:07:23 +0200

    fix: configure docker build to use WHISPER_MODEL env

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -5,5 +5,3 @@ from whisper import _download, _MODELS  # type: ignore
 if __name__ == "__main__":
     model_name = sys.argv[1].strip()
     _download(_MODELS[model_name], "/models/", False)
-    # if model_name != "large":
-    #     _download(_MODELS[f"{model_name}.en"], "/models/", False)
